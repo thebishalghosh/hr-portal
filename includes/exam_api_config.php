66ee641bcb29ed2b35481bc9813ec52151486a91
Bishal Ghosh <thebishalghosh@gmail.com>
2025-12-26 13:19:11 +0530

    updated

--- includes/exam_api_config.php
+++ includes/exam_api_config.php
@@ -1,5 +1,5 @@
 <?php
 // Exam Portal API Configuration
-$exam_api_url = 'http://localhost/exam/public/api/candidate-exams.php';
+$exam_api_url = 'https://exam.travarsa.net/api/candidate-exams';
 $exam_api_key = 'a_new_secret_key_for_exam_portal';
 ?>
